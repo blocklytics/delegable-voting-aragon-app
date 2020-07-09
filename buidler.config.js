ee862710a58c7c8595b8729f40c0a951e538447b
Pyython <vogonpoetry06@gmail.com>
2020-07-09 21:06:08 +0700

    added rinkeby to networks

--- buidler.config.js
+++ buidler.config.js
@@ -1,5 +1,8 @@
 const { usePlugin } = require('@nomiclabs/buidler/config')
 const hooks = require('./scripts/buidler-hooks')
+const INFURA_KEY = process.env.INFURA_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 usePlugin('@aragon/buidler-aragon')
 
@@ -9,7 +12,12 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://localhost:8545',
-    },
+    }, 
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/" + INFURA_KEY,
+      chainId: 4,
+      accounts: [PRIVATE_KEY]
+    }
   },
   solc: {
     version: '0.4.24',
@@ -20,7 +28,7 @@ module.exports = {
   },
   // Etherscan plugin configuration. Learn more at https://github.com/nomiclabs/buidler/tree/master/packages/buidler-etherscan
   etherscan: {
-    apiKey: '', // API Key for smart contract verification. Get yours at https://etherscan.io/apis
+    apiKey: ETHERSCAN_API_KEY, // API Key for smart contract verification. Get yours at https://etherscan.io/apis
   },
   // Aragon plugin configuration
   aragon: {
